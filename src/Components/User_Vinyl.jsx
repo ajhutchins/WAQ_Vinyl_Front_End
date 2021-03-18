84c85c4c5ac370ba8ea5c1d9f429c431cf6ed47e
A.J. Hutchins <a.j.hutchins@Kates-MacBook-Air.local>
2021-03-18 11:51:07 -0400

    added the User_Vinyl.jsx file and set up the bones

---
+++ src/Components/User_Vinyl.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+let baseURL = ''
+
+if(process.env.NODE_ENV === 'development'){
+    baseURL = 'http://localhost:3003'
+  } else {
+    baseURL = 'https://whispering-everglades-63027.herokuapp.com'
+  }
+
+class UserVinyl extends Component {
+    constructor(props) {
+        super(props)
+            this.state = {
+                name: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        fetch(baseURL + '/vinyl', {
+            method: 'POST',
+            body: JSON.stringify({ name: this.state.name }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })  .then(res => res.json())
+            .then(resJson => {
+                this.props.handleAddVinyl(resJson)
+                this.setState({
+                    name: ''
+                })
+            }).catch (error => console.log({'Error': error}))
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={ this.handleSubmit }>
+                    <label htmlFor='name'>Artist Name:</label>
+                    <input type="text" name="name" id="name" onChange={ this.handleChange } placeholder="add your favorite vinyl" value={ this.state.name }/>
+                    <input type="submit" value="Add your favorite Vinyl" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default UserVinyl
