6cf20b90d177b8b59ad4486f735b30aeee687849
William Leacy <will.leacy10@gmail.com>
2021-03-18 12:59:28 -0400

    Add nav bar search for discogs api component for search results

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Wishlist from './Components/Wishlist'
 import Collection from './Components/Collection'
+import DiscogsSearchResults from './Components/DiscogsSearchResults';
+import NavBar from './Components/NavBar';
 
 let baseURL = ''
 
@@ -16,19 +18,79 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      baseURL: '',
-      apikey: 'apikey=',
+      discogsSearchValue: '',
+      baseURL: 'https://api.discogs.com/database/search?q=',
+      token: '&token=hLKyUdeGzviIupbdIpyGeAYPVXUFOSdPDKBonIQj',
       artistName: '',
-      searchURL: ''
+      searchURL: '',
+      userCollection: [],
+      userWishlist: [],
     }
+    this.handleSearchDiscogsInputChange = this.handleSearchDiscogsInputChange.bind(this);
+    this.handleSearchDiscogsSubmit = this.handleSearchDiscogsSubmit.bind(this);
+    this.searchDiscogs = this.searchDiscogs.bind(this)
   }
 
+  searchDiscogs (event) {
+    event.preventDefault()
+    console.log(this.state.discogsSearchValue)
+    this.setState({
+      answerHidden: false,
+      searchURL: ((this.state.baseURL) + (this.state.discogsSearchValue) + (this.state.token)) ,
+    }, () => {
+      fetch(this.state.searchURL)
+        .then(response => {
+          return response.json()
+        }).then(json => this.setState({
+          vinyl: json,
+        }),
+        err => console.log(err))
+    })
+  }
 
+  handleSearchDiscogsInputChange(event) {
+    this.setState({discogsSearchValue: event.target.value});
+  }
+  
+  handleSearchDiscogsSubmit(event) {
+    event.preventDefault();
+    this.searchDiscogs(event);
+  }
+  
+  addVinylToCollection(vinyl) {
+    this.setState({
+      userCollection: [vinyl, ...this.state.userCollection],
+    })
+  }
+  
+  addVinylToWishlist(vinyl) {
+    this.setState({
+      userWishlist: [vinyl, ...this.state.userWishlist],
+    })
+  }
 
   render() {
     return(
       <div className='container'>
         <h1>WAQ VINYL</h1>
+        <div className="users-nav-content-container">
+        <NavBar />
+        <form className="discogs-search-form-container" onSubmit={this.handleSearchDiscogsSubmit}>
+            <label>
+              Name:
+              <input type="text" value={this.state.discogsSearchValue} onChange={this.handleSearchDiscogsInputChange} />
+            </label>
+            <button className="" onClick={this.searchDiscogs} >Search</button>
+      </form>
+      {(this.state.vinyl)
+            ? <DiscogsSearchResults 
+            addVinylToCollection={ (vinyl) => this.addVinylToCollection(vinyl) }
+            addVinylToWishlist={ (vinyl) => this.addVinylToWishlist(vinyl) }
+            vinyl={this.state.vinyl} 
+            />
+            : ''
+          }
+          </div>
       </div>
     )
   }
