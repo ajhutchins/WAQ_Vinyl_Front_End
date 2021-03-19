1d428a6a739720e801435625b4e640f23b8283de
A.J. Hutchins <a.j.hutchins@Kates-MacBook-Air.local>
2021-03-19 08:29:12 -0400

    cleaning up css changes

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ getUserCollection() {
         <div className="components-container">
         <form className="discogs-search-form-container" onSubmit={this.handleSearchDiscogsSubmit}>
             <label>
-              Name:
+              <h2>Name:</h2>
               <input type="text" value={this.state.discogsSearchValue} onChange={this.handleSearchDiscogsInputChange} />
             </label>
             <button className="search-button" onClick={this.searchDiscogs} >Search</button>
